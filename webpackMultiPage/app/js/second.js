58b3b18fcb117538109e655258d871d782002991
Porco_Mar <990135141@qq.com>
2018-05-18 14:37:47 +0800

    新的提交

--- webpackMultiPage/app/js/second.js
+++ webpackMultiPage/app/js/second.js
@@ -1,10 +1,11 @@
 console.log('second.js');
-import $ from 'jquery'; 
+import '../css/second.scss'
 const utils = require('./common');
 
-utils.requestData(utils.brandDetailHost(),'/h5_api/queryByStaffId',{id:utils.getQueryString('customerServiceId')},'post').then((res) => {
-  console.log(res);
-  if(res.code==='0'){
-    $('body').html(JSON.stringify(res.result));
-  }
-});
+$('#sec').animate({'left':'1000px','fontSize':'60px'},2000).animate({'left':'0px',"fontSize": "18px"},1000)
+// utils.requestData(utils.brandDetailHost(),'/h5_api/queryByStaffId',{id:utils.getQueryString('customerServiceId')},'post').then((res) => {
+//   console.log(res);
+//   if(res.code==='0'){
+//     $('body').html(JSON.stringify(res.result));
+//   }
+// });
