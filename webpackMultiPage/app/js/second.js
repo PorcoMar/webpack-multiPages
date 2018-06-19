96724a96f82f0c7b11cb80d7ed270227ff3299a6
Porco_Mar <990135141@qq.com>
2018-06-19 20:49:29 +0800

    node版本的的问题新增readme.md

--- webpackMultiPage/app/js/second.js
+++ webpackMultiPage/app/js/second.js
@@ -1,11 +1,12 @@
+const $ = require('jquery'); //不用声明也可以，但在lint检查下不声明就会报错
 console.log('second.js');
-import '../css/second.scss'
-const utils = require('./common');
-
-$('#sec').animate({'left':'1000px','fontSize':'60px'},2000).animate({'left':'0px',"fontSize": "18px"},1000)
+import '../css/second.scss';
+$('#sec').animate({'left':'1000px','fontSize':'60px'},2000).animate({'left':'0px','fontSize': '18px'},1000);
+// const utils = require('./common'); //这种声明了不使用也会报错
 // utils.requestData(utils.brandDetailHost(),'/h5_api/queryByStaffId',{id:utils.getQueryString('customerServiceId')},'post').then((res) => {
 //   console.log(res);
 //   if(res.code==='0'){
 //     $('body').html(JSON.stringify(res.result));
 //   }
 // });
+
